b1641ed3cf35cdcacee2578b1702fbe2ed4a1a6d
dulalchakrabarti <dulalchakrabarti@gmail.com>
2023-07-15 06:14:45 +0530

    Update exp_informer.py

--- exp/exp_informer.py
+++ exp/exp_informer.py
@@ -92,6 +92,7 @@ class Exp_Informer(Exp_Basic):
             freq=freq,
             cols=args.cols
         )
+        print(root_path, data_path, cols)
         print(flag, len(data_set))
         data_loader = DataLoader(
             data_set,
